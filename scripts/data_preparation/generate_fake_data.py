26ae1b48589a7e70cdd1c819c64347a0fe9e6f99
Matthew Scheeres <matthewscheeres@gmail.com>
2024-03-18 14:54:31 +0100

    Updated fake datasets

--- scripts/data_preparation/generate_fake_data.py
+++ scripts/data_preparation/generate_fake_data.py
@@ -5,6 +5,7 @@ This data preparation script generates a fake dataset of patient data and saves 
 import pandas as pd
 import random
 import faker
+from sklearn.model_selection import train_test_split
 
 # Set the number of samples
 num_samples = 1000
@@ -12,13 +13,15 @@ num_samples = 1000
 # Initialize a Faker instance for generating fake data
 fake = faker.Faker('nl_NL')
 
+options = ['0', '1', '2']
+
 # Generate random data
 data = {
     'Patnr': [random.randint(1000000, 1040000) for _ in range(num_samples)],
     'Praktnr': [random.randint(1000, 1083) for _ in range(num_samples)],
-    'Koorts': [random.choice(['-', '0', '1']) for _ in range(num_samples)],
-    'Hoesten': [random.choice(['-', '0', '1']) for _ in range(num_samples)],
-    'Kortademigheid': [random.choice(['-', '0', '1']) for _ in range(num_samples)],
+    'Koorts': [random.choice(options) for _ in range(num_samples)],
+    'Hoesten': [random.choice(options) for _ in range(num_samples)],
+    'Kortademigheid': [random.choice(options) for _ in range(num_samples)],
 }
 
 # Create a DataFrame
@@ -30,5 +33,11 @@ df['DEDUCE_omschrijving'] = df.apply(lambda row: fake.text().replace('\n', ' ') 
                                       (' Hoesten' if row['Hoesten'] == '1' else ' geen Hoesten' if row['Hoesten'] == '0' else '') +
                                       (' Kortademigheid' if row['Kortademigheid'] == '1' else ' geen Kortademigheid' if row['Kortademigheid'] == '0' else ''), axis=1)
 
+# Split into train and test sets
+train_size = int(0.8 * len(df))
+train_set = df[:train_size]
+test_set = df[train_size:]
+
 # Save the DataFrame to a CSV file
-df.to_csv('data/raw/patient_data/example_dataset.csv', index=False)
+train_set.to_csv('data/raw/patient_data/fake_train.csv', index=False)
+test_set.to_csv('data/raw/patient_data/fake_test.csv', index=False)
