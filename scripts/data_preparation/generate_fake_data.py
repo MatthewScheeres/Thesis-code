bc352b0ab664a4d888c84ab2e345ebd494d28173
Matthew Scheeres <46484495+MatthewScheeres@users.noreply.github.com>
2024-04-02 16:55:23 +0200

    updating fake data

--- scripts/data_preparation/generate_fake_data.py
+++ scripts/data_preparation/generate_fake_data.py
@@ -33,11 +33,26 @@ df['DEDUCE_omschrijving'] = df.apply(lambda row: fake.text().replace('\n', ' ') 
                                       (' Hoesten' if row['Hoesten'] == '1' else ' geen Hoesten' if row['Hoesten'] == '0' else '') +
                                       (' Kortademigheid' if row['Kortademigheid'] == '1' else ' geen Kortademigheid' if row['Kortademigheid'] == '0' else ''), axis=1)
 
-# Split into train and test sets
-train_size = int(0.8 * len(df))
-train_set = df[:train_size]
-test_set = df[train_size:]
+def divide_set(df: pd.DataFrame, k: int = 5) -> list[pd.DataFrame]:
+    """Divides the provided set into k equally sized subsets, then returns them in a list.
+
+    Args:
+        df (pd.DataFrame): Dataframe to use.
+        k (int, optional): Amount of subsets. Defaults to 5.
+
+    Returns:
+        list[pd.DataFrame]: A list containing the chunked-up subsets of the original DataFrame.
+    """
+    subset_size = len(df) // k
+    subsets = []
+    for i in range(k):
+        subset = df[i*subset_size:(i+1)*subset_size]
+        subsets.append(subset)
+    return subsets
+    
+test_sets = divide_set(df, k=5)
 
 # Save the DataFrame to a CSV file
-train_set.to_csv('data/raw/patient_data/fake_train.csv', index=False)
-test_set.to_csv('data/raw/patient_data/fake_test.csv', index=False)
+df.to_csv('data/raw/patient_data/fake_set_complete.csv', index=False)
+for i, test_set in enumerate(test_sets):
+    test_set.to_csv(f'data/raw/patient_data/fake_test_{i+1}.csv', index=False)
