efc3c8116d463f55d35bc162960244712e162f04
Matthew Scheeres <matthewscheeres@gmail.com>
2024-04-26 15:26:17 +0200

    Update compose_datasets.py

--- scripts/data_preparation/compose_datasets.py
+++ scripts/data_preparation/compose_datasets.py
@@ -2,10 +2,23 @@ import pandas as pd
 import pyreadstat
 
 # Load the CSV file
-df_csv = pd.read_csv('Element_freetext_SO.csv', sep='|', usecols=["Patnr", "DEDUCE_omschrijving,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"])
+df_ehr = pd.read_csv('Element_freetext_SO.csv', sep='|', usecols=["Patnr", "DEDUCE_omschrijving,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"])
 
 # Load the SPSS file
+symptoms = ["Temp", "Hoesten", "Dyspnoe"]
+keep_columns = ["Patnr"] + symptoms
+
 df_spss, meta = pyreadstat.read_sav('Element_sample_freetext.sav')
+df_symptoms = df_spss[keep_columns]
+
+# Parse Koorts Data
+df_symptoms['Koorts'] = df_symptoms['Temp'].apply(lambda x: 0 if x in [0, 1] else 1 if x == 2 else 2)
+
+df_symptoms[: , symptoms] = df_symptoms[symptoms].fillna(2)
+df_symptoms = df_symptoms.rename(columns={'Dyspnoe': 'Kortademigheid', 'Temp': 'Koorts'})
 
 # Merge the dataframes
-df = pd.merge(df_csv, df_spss, on='Patnr')
+df = pd.merge(df_ehr, df_spss, on='Patnr')
+
+
+df.to_csv('output.csv', index=False)
